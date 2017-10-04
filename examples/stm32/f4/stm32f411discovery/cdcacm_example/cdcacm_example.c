3bd07baca8c9652c870ac9481e46da35bd09706c
Federico Ruiz Ugalde <memeruiz@gmail.com>
2017-10-04 02:59:44 -0600

    Support for stm32f411discovery

---
+++ examples/stm32/f4/stm32f411discovery/cdcacm_example/cdcacm_example.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2013 ARCOS-Lab Universidad de Costa Rica
+ * Author: Federico Ruiz Ugalde <memeruiz@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3-plus/newlib/syscall.h>
+#include "cdcacm_example.h"
+#include <libopencm3-plus/cdcacm_one_serial/cdcacm.h>
+#include <stdio.h>
+#include <libopencm3-plus/utils/misc.h>
+#include <libopencm3-plus/stm32f4discovery/leds.h>
+#include <limits.h>
+#include <stdbool.h>
+
+void leds_init(void) {
+	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPDEN);
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12| GPIO13| GPIO14| GPIO15);
+}
+
+void system_init(void) {
+  rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_84MHZ]);
+  leds_init();
+  cdcacm_init();
+}
+
+int main(void)
+{
+  system_init();
+  char cmd_s[50]="";
+  char cmd[10]="";
+  char confirm[10]="";
+  int i, j;
+  int c=0;
+  int n_char=0;
+
+  setvbuf(stdin,NULL,_IONBF,0); // Sets stdin in unbuffered mode (normal for usart com)
+  setvbuf(stdout,NULL,_IONBF,0); // Sets stdin in unbuffered mode (normal for usart com)
+
+  while (poll(stdin) > 0) {
+    printf("Cleaning stdin\n");
+    getc(stdin);
+  }
+
+  while (1){
+    printf("Test\n");
+    if ((poll(stdin) > 0)) {
+      i=0;
+      if (poll(stdin) > 0) {
+    	c=0;
+    	while (c!='\r') {
+    	  c=getc(stdin);
+    	  i++;
+    	  putc(c, stdout);
+    	}
+      }
+    }
+  }
+}
